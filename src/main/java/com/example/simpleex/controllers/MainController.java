1ac839c64a67d6ee6d22a1d0e10f33afd2e17032
Sharton <sharton.msk@gmail.com>
2018-12-25 17:27:33 +0300

    hello

--- src/main/java/com/example/simpleex/controllers/MainController.java
+++ src/main/java/com/example/simpleex/controllers/MainController.java
@@ -1,5 +1,15 @@
 package com.example.simpleex.controllers;
 
-public class MainController {
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+    @GetMapping
+    public String main(String name, Model model){
+        model.addAttribute("name", name);
+        return "/main";
+    }
 }
