db96258d3b8bcd7248b269c58301b939056286e2
Sharton <sharton.msk@gmail.com>
2018-12-26 12:56:13 +0300

    hello

--- src/main/java/com/example/simpleex/repos/UserRep.java
+++
@@ -1,8 +0,0 @@
-package com.example.simpleex.repos;
-
-import com.example.simpleex.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRep extends JpaRepository {
-    User findByUsername(String username);
-}
